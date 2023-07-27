5ad4ea82778f45812b9eb95f434db8ad1f823fdf
Tries Augustrianto <triesatrianto@gmail.com>
2023-07-27 09:48:32 +0700

    Day 2

--- README.md
+++ README.md
@@ -5,6 +5,20 @@ Adalah sebuah progres selama 7 hari dan disetiap hari akan memposting / push seb
 ## List
 1. Day 1
 Soal : membuat suatu code untuk memfilter atau menyaring dari sebuah kalimat agar nanti di tampilkan tanpa huruf vokal
-kalimat = "hallo hari ini saya belajar filter"
-output =
+kalimat : "hallo hari ini saya belajar filter"
+output :
 hll hr n sy bljr fltr
+
+2. Day 2
+Soal : tentukan nilai raport dan kelulusan berdasarkan nilai angka yang sudah ditentukan
+nilai angka : [70, 89, 98, 45, 76, 34, 50,113, -2]
+output :
+student 1 = 70 atau B dan dinyatakan : LULUS
+student 2 = 89 atau A dan dinyatakan : LULUS
+student 3 = 98 atau A dan dinyatakan :  LULUS
+student 4 = 45 atau C dan dinyatakan : TIDAK LULUS
+student 5 = 76 atau B+ dan dinyatakan : LULUS
+student 6 = 34 atau C dan dinyatakan : TIDAK LULUS
+student 7 = 50 atau C dan dinyatakan : TIDAK LULUS
+student 8 = 113 atau Nilai tidak terdaftar dan dinyatakan : TIDAK LULUS
+student 9 = -2 atau Nilai tidak terdaftar dan dinyatakan : TIDAK LULUS
--- index.js
+++ index.js
@@ -28,3 +28,45 @@ console.log(
 output : 
 hll hr n sy bljr fltr
 */
+
+// DAY 2
+// Menentukan nilai raport berdasarkan nilai angka 
+
+const result = [70, 89, 98, 45, 76, 34, 50,113, -2]
+let raport = () => {
+for (let i = 0; i < result.length; i++){
+    let indexResult = result[i]
+    let nilaiResult =''
+    if (indexResult <= 64 && indexResult >= 0){
+        nilaiResult = 'C'
+    }else if (indexResult <= 74 && indexResult >= 65){
+        nilaiResult = 'B'
+    }else if(indexResult <= 84 && indexResult >= 75){
+        nilaiResult = 'B+'
+    }else if(indexResult <= 100 && indexResult >= 85){
+        nilaiResult = 'A'
+    }
+     else {
+        nilaiResult = 'Nilai tidak terdaftar'
+    }
+
+    const kelulusan = nilaiResult === 'A' ||
+     nilaiResult === 'B+' || nilaiResult === 'B' || nilaiResult === 'c' ? "LULUS" : "TIDAK LULUS" 
+    console.log(`student ${i +1} = ${indexResult} atau ${nilaiResult} dan dinyatakan : 
+    ${kelulusan}`)
+   
+}}
+raport()
+
+/*
+output :
+student 1 = 70 atau B dan dinyatakan : LULUS
+student 2 = 89 atau A dan dinyatakan : LULUS
+student 3 = 98 atau A dan dinyatakan :  LULUS
+student 4 = 45 atau C dan dinyatakan : TIDAK LULUS
+student 5 = 76 atau B+ dan dinyatakan : LULUS
+student 6 = 34 atau C dan dinyatakan : TIDAK LULUS
+student 7 = 50 atau C dan dinyatakan : TIDAK LULUS
+student 8 = 113 atau Nilai tidak terdaftar dan dinyatakan : TIDAK LULUS
+student 9 = -2 atau Nilai tidak terdaftar dan dinyatakan : TIDAK LULUS
+*/
